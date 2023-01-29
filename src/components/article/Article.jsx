88fefb1ca59bc4711838efc86a0fd0d4ea7767a5
Prince Gupta <princegupta1586@gmail.com>
2023-01-29 18:09:31 +0530

    Create Blog Section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,5 +1,18 @@
 import "./article.css";
 
-export default function Article() {
-  return <div>Article</div>;
+export default function Article({ imgUrl }) {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blogImage" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>Sep 26, 2021</p>
+          <h3>GPT-3 and Open AI is the future. Let us exlore how it is?</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,26 @@
 import "./blog.css";
+import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt3__blog section__padding">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={blog01} />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article imgUrl={blog02} />
+          <Article imgUrl={blog03} />
+          <Article imgUrl={blog04} />
+          <Article imgUrl={blog05} />
+        </div>
+      </div>
+    </div>
+  );
 }
