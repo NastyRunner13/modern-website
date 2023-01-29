094dac841612622d942226a04a271c180adbe90f
Prince Gupta <princegupta1586@gmail.com>
2023-01-29 17:06:15 +0530

    Create CTA section

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,5 +1,15 @@
 import "./cta.css";
 
 export default function Cta() {
-  return <div>Cta</div>;
+  return (
+    <div className="gpt3__cta section__margin">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 }
