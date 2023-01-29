e1e93d5bd6105278e39dc9da543e25afdfa4c599
Prince Gupta <princegupta1586@gmail.com>
2023-01-29 15:40:40 +0530

    Create WhatisGPT3 Section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,5 +1,16 @@
 import "./feature.css";
 
-export default function Feature() {
-  return <div>Feature</div>;
+export default function Feature({ title, text }) {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <div />
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 }
